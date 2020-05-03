ea8f582ad4682149d5e9d065496d1301971d4a6d
Matti Paksula <matti.paksula@iki.fi>
2020-05-03 12:12:03 +0300

    :init:

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM mattipaksula/reflux:1.13.10-alpine3.11
+
+COPY app .
---
+++ app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello")
+	})
+
+	router.Run(":8080")
+}
